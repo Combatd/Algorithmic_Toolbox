c53a201180df307229046e6989a9465df246bcd2
Combatd <combatdstar@gmail.com>
2020-09-27 06:47:56 -0700

    SReturn a list of fibonacci numbers in fibonacci.py

--- week2/fibonacci.py
+++ week2/fibonacci.py
@@ -1,11 +1,22 @@
 # Uses python3
 def calc_fib(n):
     if (n <= 1):
-        return n
+        return [0]
     elif (n == 2):
-        return 1
-
-    return (n - 1) + (n - 2)
+        return [0, 1]
+    
+    f1 = 0
+    f2 = 1
+    fibArray = [f1, f2] # we will try to grab the last number
+    while f2 < n:
+        original_f1 = 0 + f1
+        f1 = f2
+        f2 = original_f1 + f2 # needs to add the previous number
+        fibArray.append(f2)
+    print(fibArray)
+    
+    # return the second to number in the list!
+    return fibArray[len(fibArray) - 2]
 
 n = int(input())
 print(calc_fib(n))
