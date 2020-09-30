1f905c4fd9e7e3065bd2a4645bd16f6a78ea052b
Combatd <combatdstar@gmail.com>
2020-09-30 05:33:58 -0700

    Whiteboard #get_optimal_value psuedocode

--- week3/fractional_knapsack.py
+++ week3/fractional_knapsack.py
@@ -1,6 +1,27 @@
 # Uses python3
 import sys
 
+'''
+Greedy Algorithm
+* While capacity is not full
+* Choose item i with maximum vi / wi (volume / weight)
+* if item fits into capacity, take all of it
+* Otherwise, take enough of the item to fill to total capacity
+* Return value and amounts of each item
+
+Amount <-- [0,0,0,0,0], Value <- 0
+
+repeat n times:
+    if Weight = 0:
+        return (V, A)
+    select i with Wi > 0 and max Vi/Wi
+    a <- min(Wi, W)
+    V <- V + a(Vi/Wi)
+    ** Change the value of the variables! **
+    Wi <- Wi - a, A[i] <- A[i] + a, W <- V 
+return (V, A)
+'''
+
 def get_optimal_value(capacity, weights, values):
     value = 0.
     # write your code here
