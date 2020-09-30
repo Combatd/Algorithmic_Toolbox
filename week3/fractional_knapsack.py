e0ad4ce8918f79a3a3dd4ba955abf99f47b0f5ca
Combatd <combatdstar@gmail.com>
2020-09-30 08:57:46 -0700

    Import car_ fueling problem

--- week3/fractional_knapsack.py
+++ week3/fractional_knapsack.py
@@ -38,16 +38,44 @@ for i from 1 to n:
     Wi <- Wi - a, A[i] <- A[i] + a, W <- V - a
 return (V, A)
 
+Array A will be filled with amounts of items starting with values 0
+
 * Each iteration is O(1) Constant Time
 * If we sort the items first, that takes O(n)
 * Sort + Knapsack is O(nlogn) Linear Logarithmic time as it is always less than O(n)
 '''
 
 def get_optimal_value(capacity, weights, values):
-    value = 0.
+    value = 0
     # write your code here
 
+    # print(values)
+    # print(weights)
+    # sorted_weights = sorted(weights)
+    # sorted_values = sorted(values)
+        
+    amounts = []
+    idx = 0
+    for item in range(0, len(weights) - 1):
+        amounts.append([values[idx]])
+        idx += 1
+
+    return amounts
+    
+    W = 0 + capacity
+    i = 0
+    # while i < len(weights) - 1:
+    #     i += 1
+    #     if W == 0:
+    #         return value
+    #     amount = min(values[i]/weights[i]) # round the float
+    #     values += amount
+    #     W = W - amount
+
     return value
+
+        
+
 
 
 if __name__ == "__main__":
